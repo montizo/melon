b3ccfbb3e8cf84cf9f5a5d72bc3ceb75772844c5
montizo <198330536+montizo@users.noreply.github.com>
2025-03-01 10:17:31 +1100

    refactor: move checkRateLimit to base layout

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,17 +1,8 @@
 import { getCurrentSession } from "@/lib/auth/session";
 import LoginForm from "@/ui/auth/Login";
-import { checkRateLimit } from "@/utils/checkRateLimit";
-import rateLimit from "@/utils/rateLimit";
-import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function LoginPage() {
-  const { isRateLimited, message } = await checkRateLimit("GET", 100, 60);
-
-  if (isRateLimited) {
-    return <h1>{message}</h1>;
-  }
-
   const session = await getCurrentSession();
   if (session.userId) {
     redirect("/");
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,17 +2,8 @@ import { getCurrentSession } from "@/lib/auth/session";
 import { getUserById } from "@/lib/auth/user";
 import SignupForm from "@/ui/auth/Signup";
 import { redirect } from "next/navigation";
-import { headers } from "next/headers";
-import rateLimit from "@/utils/rateLimit";
-import { checkRateLimit } from "@/utils/checkRateLimit";
 
 export default async function SignupPage() {
-  const { isRateLimited, message } = await checkRateLimit("GET", 100, 60);
-
-  if (isRateLimited) {
-    return <h1>{message}</h1>;
-  }
-
   const session = await getCurrentSession();
 
   if (session.userId) {
