fe4f2f645fbe942d87974911f085d5ed8e175be6
montizo <198330536+montizo@users.noreply.github.com>
2025-03-01 17:44:31 +1100

    feat: add handlePaste function to verify-email

--- src/ui/auth/VerifyEmail.tsx
+++ src/ui/auth/VerifyEmail.tsx
@@ -1,23 +1,24 @@
 "use client";
 
-import verifyEmailAction from "@/app/(auth)/verify-email/actions";
+import React, { useEffect, useRef, useState } from "react";
+import { startTransition, useActionState } from "react";
 import Form from "@/components/Form";
-import {
-  startTransition,
-  useActionState,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
+import verifyEmailAction from "@/app/(auth)/verify-email/actions";
+
+type CodeState = string[];
+
+interface InputRefs {
+  current: HTMLInputElement[];
+}
 
 export default function VerifyEmail() {
-  const [state, action, isLoading] = useActionState(verifyEmailAction, null);
+  const [state, action] = useActionState(verifyEmailAction, null);
   const [code, setCode] = useState(new Array(8).fill(""));
   const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
   const error = "";
 
-  const handleChange = (index: number, value: string) => {
-    const newCode = [...code];
+  const handleChange = (index: number, value: string): void => {
+    const newCode: CodeState = [...code];
 
     newCode[index] = value;
     setCode(newCode);
@@ -25,6 +26,24 @@ export default function VerifyEmail() {
     if (value && index < 7) {
       inputRefs.current[index + 1]?.focus();
     }
+  };
+
+  const handlePaste = (
+    e: React.ClipboardEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    const pastedText = e.clipboardData.getData("Text");
+    const newCode: CodeState = [...code];
+
+    const pastedCode = pastedText.slice(0, 8).split("");
+    for (let i = 0; i < pastedCode.length && i + index < 8; i++) {
+      newCode[index + i] = pastedCode[i];
+    }
+    setCode(newCode);
+
+    const lastFilledIndex = newCode.findLastIndex((digit) => digit !== "");
+    const focusIndex = lastFilledIndex < 7 ? lastFilledIndex + 1 : 7;
+    inputRefs.current[focusIndex]?.focus();
   };
 
   const handleKeyDown = (
@@ -38,7 +57,6 @@ export default function VerifyEmail() {
 
   const handleSubmit = () => {
     const verificationCode = code.join("");
-
     startTransition(async () => {
       await action(verificationCode);
     });
@@ -66,6 +84,7 @@ export default function VerifyEmail() {
             value={digit}
             onChange={(e) => handleChange(index, e.target.value)}
             onKeyDown={(e) => handleKeyDown(index, e)}
+            onPaste={(e) => handlePaste(e, index)} // Use onPaste here
             className="border-[1.5px] text-[#fafafa] outline-none focus:ring-3 ring-[#242424] rounded-md duration-200 bg-[#181818] border-[#222222] placeholder-[#4d4d4d] focus:border-[#444444] w-10 h-10 text-center"
           />
         ))}
