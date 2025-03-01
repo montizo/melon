df9443581d2a0f91aaa8efe546cdb3c8506c4661
montizo <198330536+montizo@users.noreply.github.com>
2025-03-02 10:21:47 +1100

    feat: add button disabled to login

--- src/ui/auth/Login.tsx
+++ src/ui/auth/Login.tsx
@@ -4,14 +4,28 @@ import Input from "@/components/Input";
 import loginAction from "@/app/(auth)/login/actions";
 import { emailSchema } from "@/lib/validation";
 import Form from "@/components/Form";
+import { useEffect, useState } from "react";
 
 export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [hasErrors, setHasErrors] = useState(false);
+
+  useEffect(() => {
+    if (emailSchema.safeParse(email).success === true) {
+      setHasErrors(false);
+    } else {
+      setHasErrors(true);
+      return;
+    }
+  }, [email]);
+
   return (
     <Form
       formAction={loginAction}
       title="Welcome back"
       subTitle="Login to your account"
       buttonText="Login"
+      buttonDisabled={hasErrors}
       footer={{
         text: "Don't have an account?",
         linkText: "Sign Up",
@@ -24,6 +38,7 @@ export default function LoginForm() {
         name="email"
         placeholder="you@domain.xyz"
         validation={emailSchema}
+        setExternalUseState={(value) => setEmail(value)}
       />
       <Input
         label="Password"
