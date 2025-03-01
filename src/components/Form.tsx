a47b41a7718be0687787367a861247edec69e3ec
montizo <198330536+montizo@users.noreply.github.com>
2025-03-01 17:06:12 +1100

    refactor: move login form into seperate form component

---
+++ src/components/Form.tsx
@@ -0,0 +1,52 @@
+import React, { ReactNode, useActionState } from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+export default function Form({
+  formAction,
+  title,
+  subTitle,
+  buttonText,
+  children,
+  footer,
+}: {
+  formAction: any;
+  title: string;
+  subTitle: string;
+  buttonText: string;
+  children: ReactNode;
+  footer: {
+    text: string;
+    linkText: string;
+    linkTo: string;
+  };
+}) {
+  const [state, action, isLoading] = useActionState(formAction, null);
+
+  return (
+    <div className="bg-[#181818] border-[1.5px] border-[#222222] p-8 rounded-2xl w-full max-w-md grid gap-8">
+      <div className="grid gap-1">
+        <h2 className="font-semibold text-3xl text-[#fafafa]">{title}</h2>
+        <p>{subTitle}</p>
+      </div>
+      <form action={action} className="grid gap-4">
+        {children}
+        <motion.button
+          whileHover={{ scale: 1.02 }}
+          whileTap={{ scale: 0.98 }}
+          type="submit"
+          disabled={isLoading}
+          className="cursor-pointer px-3 py-1 rounded-md font-semibold bg-[#822929] border-[#9f3a3a] border-1 hocus:brightness-125 duration-300"
+        >
+          {buttonText}
+        </motion.button>
+      </form>
+      <p>
+        {footer.text}{" "}
+        <Link href={footer.linkTo} className="text-[#df3f3f] hocus:underline">
+          {footer.linkText}
+        </Link>
+      </p>
+    </div>
+  );
+}
--- src/ui/auth/Login.tsx
+++ src/ui/auth/Login.tsx
@@ -1,51 +1,36 @@
 "use client";
 
-import { motion } from "framer-motion";
 import Input from "@/components/Input";
-import Link from "next/link";
-import { useActionState } from "react";
 import loginAction from "@/app/(auth)/login/actions";
 import { emailSchema } from "@/lib/validation";
+import Form from "@/components/Form";
 
 export default function LoginForm() {
-  const [state, action, isLoading] = useActionState(loginAction, null);
-
   return (
-    <div className="bg-[#181818] border-[1.5px] border-[#222222] p-8 rounded-2xl w-full max-w-md grid gap-8">
-      <div className="grid gap1">
-        <h2 className="font-semibold text-3xl text-[#fafafa]">Welcome back</h2>
-        <p>Login to your account</p>
-      </div>
-      <form action={action} className="grid gap-4">
-        <Input
-          label="Email"
-          type="text"
-          name="email"
-          placeholder="you@domain.xyz"
-          validation={emailSchema}
-        />
-        <Input
-          label="Password"
-          type="password"
-          name="password"
-          placeholder="●●●●●●"
-        />
-        <motion.button
-          whileHover={{ scale: 1.02 }}
-          whileTap={{ scale: 0.98 }}
-          type="submit"
-          disabled={isLoading}
-          className="cursor-pointer px-3 py-1 rounded-md font-semibold bg-[#822929] border-[#9f3a3a] border-1 hocus:brightness-125 duration-300"
-        >
-          Login
-        </motion.button>
-      </form>
-      <p>
-        Don't have an account?{" "}
-        <Link href="/signup" className="text-[#df3f3f] hocus:underline">
-          Sign Up
-        </Link>
-      </p>
-    </div>
+    <Form
+      formAction={loginAction}
+      title="Welcome back"
+      subTitle="Login to your account"
+      buttonText="Login"
+      footer={{
+        text: "Don't have an account?",
+        linkText: "Sign Up",
+        linkTo: "/signup",
+      }}
+    >
+      <Input
+        label="Email"
+        type="text"
+        name="email"
+        placeholder="you@domain.xyz"
+        validation={emailSchema}
+      />
+      <Input
+        label="Password"
+        type="password"
+        name="password"
+        placeholder="●●●●●●"
+      />
+    </Form>
   );
 }
