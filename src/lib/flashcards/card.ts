0da4eb3021624c598162a2f5198c1a062d4d4f09
montizo <198330536+montizo@users.noreply.github.com>
2025-03-05 18:45:22 +1100

    feat: add card and set methods

---
+++ src/lib/flashcards/card.ts
@@ -0,0 +1,34 @@
+import prisma from "../db/prisma/prisma";
+
+export async function createCard(
+  setId: string,
+  term: string,
+  definition: string
+) {
+  return prisma.card.create({ data: { setId, term, definition: definition } });
+}
+
+export async function getCardById(id: string) {
+  return prisma.card.findUnique({ where: { id } });
+}
+
+export async function getCardsBySetId(setId: string) {
+  return prisma.card.findMany({ where: { setId } });
+}
+
+export async function updateCard(
+  id: string,
+  term?: string,
+  definition?: string
+) {
+  return prisma.card.update({
+    where: { id },
+    data: { term, definition: definition },
+  });
+}
+
+export async function deleteCard(id: string) {
+  return prisma.card.delete({
+    where: { id },
+  });
+}
---
+++ src/lib/flashcards/set.ts
@@ -0,0 +1,37 @@
+import prisma from "../db/prisma/prisma";
+
+export async function isSetTitleTaken(
+  userId: string,
+  title: string
+): Promise<boolean> {
+  const existingSet = prisma.set.findFirst({
+    where: { userId, title: { equals: title, mode: "insensitive" } },
+  });
+
+  return existingSet !== null;
+}
+
+export async function createSet(
+  userId: string,
+  title: string,
+  description?: string
+) {
+  return prisma.set.create({
+    data: { userId, title, description: description || "" },
+  });
+}
+
+export async function getSetById(id: string) {
+  return prisma.set.findUnique({ where: { id }, include: { cards: true } });
+}
+
+export async function getSetByUserId(userId: string) {
+  return prisma.set.findMany({
+    where: { userId },
+    include: { cards: true },
+  });
+}
+
+export async function deleteSet(id: string) {
+  return prisma.set.delete({ where: { id } });
+}
